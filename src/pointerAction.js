adefa120487bcf97d19a0f1b20dcd44d3efc7137
Isaac Hagoel <sitzikh@gmail.com>
2020-10-02 21:33:21 +1000

    124 - moved the JSDOC to the exported action and added missing properties

--- src/pointerAction.js
+++ src/pointerAction.js
@@ -223,19 +223,6 @@ function cleanupPostDrop() {
     finalizingPreviousDrag = false;
 }
 
-/**
- * A Svelte custom action to turn any container to a dnd zone and all of its direct children to draggables
- * dispatches two events that the container is expected to react to by modifying its list of items,
- * which will then feed back in to this action via the update function
- *
- * @typedef {Object} Options
- * @property {Array} items - the list of items that was used to generate the children of the given node (the list used in the #each block
- * @property {string} [type] - the type of the dnd zone. children dragged from here can only be dropped in other zones of the same type, default to a base type
- * @property {number} [flipDurationMs] - if the list animated using flip (recommended), specifies the flip duration such that everything syncs with it without conflict, defaults to zero
- * @param {HTMLElement} node - the element to enhance
- * @param {Options} options
- * @return {{update: function, destroy: function}}
- */
 export function dndzone(node, options) {
     const config =  {
         items: undefined,

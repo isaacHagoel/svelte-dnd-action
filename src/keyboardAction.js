e1df5debb2a3be5fb1498dc82ddd3e8b3d5be4af
Isaac Hagoel <sitzikh@gmail.com>
2021-01-15 15:19:10 +1100

    225 - managing the tab index of the dragged element to allow tabbing back to it even after tabbing to external elements

--- src/keyboardAction.js
+++ src/keyboardAction.js
@@ -257,6 +257,7 @@ export function dndzone(node, options) {
         const children = Array.from(node.children);
         const focusedItemIdx = children.indexOf(draggableEl);
         focusedItem = draggableEl;
+        focusedItem.tabIndex = 0;
         focusedItemId = items[focusedItemIdx][ITEM_ID_KEY];
         focusedItemLabel = children[focusedItemIdx].getAttribute("aria-label") || "";
     }
@@ -317,6 +318,7 @@ export function dndzone(node, options) {
                 printDebug(() => ["focusing on", {i, focusedItemId}]);
                 // if it is a nested dropzone, it was re-rendered and we need to refresh our pointer
                 focusedItem = draggableEl;
+                focusedItem.tabIndex = 0;
                 // without this the element loses focus if it moves backwards in the list
                 draggableEl.focus();
             }

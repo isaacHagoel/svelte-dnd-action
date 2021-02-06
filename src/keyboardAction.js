fad144bd6d5689cada0af707455ed5d535443eae
Danny Andrews <lunde.andrews@gmail.com>
2021-02-06 01:25:23 -0600

    Refine heuristic

--- src/keyboardAction.js
+++ src/keyboardAction.js
@@ -170,7 +170,7 @@ export function dndzone(node, options) {
             case "Enter":
             case " ": {
                 // we don't want to affect nested input elements or clickable elements
-                if ((e.target.click !== undefined || e.target.value !== undefined || e.target.isContentEditable) && !allDragTargets.has(e.target)) {
+                if ((e.target.disabled !== undefined || e.target.href || e.target.isContentEditable) && !allDragTargets.has(e.target)) {
                     return;
                 }
                 e.preventDefault(); // preventing scrolling on spacebar
